e712851d8d8468b5e8832801415d90db9e1fad72
brandiherrera <bcjherrera@gmail.com>
2019-11-11 12:29:43 -0800

    working application without express routers

--- src/app.js
+++ src/app.js
@@ -4,16 +4,152 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const winston = require('winston')
+const uuid = require('uuid/v4')
 
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+    ? 'tiny'
+    : 'dev';
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'info.log' })
+    ]
+});
+
+if (NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
+const bookmarks = [
+    {
+        id: 0,
+        title: 'Google',
+        url: 'http://www.google.com',
+        rating: '3',
+        desc: 'Internet-related services and products.'
+    },
+    {
+        id: 1,
+        title: 'Thinkful',
+        url: 'http://www.thinkful.com',
+        rating: '5',
+        desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
+    },
+    {
+        id: 2,
+        title: 'Github',
+        url: 'http://www.github.com',
+        rating: '4',
+        desc: 'brings together the world\'s largest community of developers.'
+    }
+];
 
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json());
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next()
+})
+
+app.get('/bookmarks', (req, res) => {
+    res.json(bookmarks)
+})
+
+app.get('/bookmarks/:id', (req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(b => b.id == id);
+
+    if (!bookmark) {
+        logger.error(`Card with id ${id} not found.`)
+        return res
+            .status(404)
+            .send(`Card not found.`);
+    }
+    res.json(bookmark);
+})
+
+app.post('/bookmarks', (req, res) => {
+    const { title, url, rating, desc } = req.body;
+
+    if (!title) {
+        logger.error(`Title is required`)
+        return res
+            .status(400)
+            .send('Invalid data');
+    }
+    if (!url) {
+        logger.error(`URL is required`)
+        return res
+            .status(400)
+            .send('Invalid data');
+    }
+    if (!rating) {
+        logger.error(`Rating is required`)
+        return res
+            .status(400)
+            .send('Invalid data');
+    }
+
+    const id = uuid();
+    const bookmark = {
+        id,
+        title,
+        url,
+        rating,
+        desc
+    }
+
+    bookmarks.push(bookmark);
+
+    logger.info(`Bookmark with id ${id} created`);
+
+    res
+        .status(201)
+        .location(`http://localhost:8000/bookmark/${id}`)
+        .json(bookmark);
+})
+
+app.delete('/bookmarks/:id', (req, res) =>{
+    const { id } = req.params;
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
+
+    if(bookmarkIndex === -1) {
+        logger.error(`Bookmark with id ${id} not found`)
+        return res
+            .status(404)
+            .send('Not found');
+    }
+    // bookmarks.forEach(bookmark => {
+    // // const bookmarkId = bookmarks.forEach(bid => bid !==id);
+
+    //     const bookmarkIds = bookmark.id.filter(bid => bid !== id);
+    //     bookmark.bookmarkIds = bookmarkIds;
+    //     debugger
+    // })
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Card with id ${id} deleted.`);
+
+    res
+        .status(204)
+        .end();
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
