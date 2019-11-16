3960c4f16b41e5b42943a098f5db4434af74ec4c
brandiherrera <bcjherrera@gmail.com>
2019-11-15 17:35:18 -0800

    POST and DELETE refactored with tests

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -3,6 +3,7 @@ const logger = require('./logger')
 
 function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization')
+  console.log(authToken)
   logger.error(`Unauthorized request to path: ${req.path}`)
 
   if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
--- test/bookmarks.fixtures.js
+++ test/bookmarks.fixtures.js
@@ -1,36 +1,56 @@
 function makeBookmarksArray() {
-    return [
-        {
-            id: 1,
-            url: 'https://www.google.com',
-            title: 'First test post!',
-            description: 'How-to',
-            rating: 5
-          },
-          {
-            id: 2,
-            url: 'https://www.google.com',
-            title: 'Second test post!',
-            description: 'News',
-            rating: 2
-          },
-          {
-            id: 3,
-            url: 'https://www.google.com',
-            title: 'Third test post!',
-            description: 'Listicle',
-            rating: 3
-          },
-          {
-            id: 4,
-            url: 'https://www.google.com',
-            title: 'Fourth test post!',
-            description: 'Story',
-            rating: 4
-          },
-        ];
-      }
-      
-  module.exports = {
-    makeBookmarksArray,
+  return [
+    {
+      id: 1,
+      url: 'https://www.google.com',
+      title: 'First test post!',
+      description: 'How-to',
+      rating: 5
+    },
+    {
+      id: 2,
+      url: 'https://www.google.com',
+      title: 'Second test post!',
+      description: 'News',
+      rating: 2
+    },
+    {
+      id: 3,
+      url: 'https://www.google.com',
+      title: 'Third test post!',
+      description: 'Listicle',
+      rating: 3
+    },
+    {
+      id: 4,
+      url: 'https://www.google.com',
+      title: 'Fourth test post!',
+      description: 'Story',
+      rating: 4
+    },
+  ];
+}
+
+function makeMaliciousBookmark() {
+  const maliciousBookmark = {
+    id: 911,
+    title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    url: 'https://www.hackers.com',
+    description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+    rating: 1,
   }
+  const expectedBookmark = {
+    ...maliciousBookmark,
+    title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+    description: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+  }
+  return {
+    maliciousBookmark,
+    expectedBookmark,
+  }
+}
+
+module.exports = {
+  makeBookmarksArray,
+  makeMaliciousBookmark,
+}
